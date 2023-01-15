a13091e3f9cf79a39a6d62864f10bb19fa529f5e
SvitlanaKi <118849159+SvitlanaKi@users.noreply.github.com>
2023-01-15 19:13:14 +0200

    end

--- css/modal.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-<script>
-    const refs={
-        openModalBtn: document.querySelector("data-open-modal"),
-    closeModalBtn: document.querySelector("data-close-modal"),
-    backdrop: document.querySelector("data-backdrop")
-    }
-</script>
